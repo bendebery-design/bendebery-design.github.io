09a39db9c6b143b6deef34bbc2e0bca21e5605d5
Leonid <bolwoy97@gmail.com>
2024-09-09 17:04:08 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import img from './image.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={img} className="App-logo" alt="logo" />
+        <p>
+          Bendebery design
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
